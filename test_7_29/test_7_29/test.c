231b24b1d81c0830277042ff03bb8ff2b52ff7f3
Guoliu <820267815@qq.com>
2021-07-30 18:34:33 +0800

    2021.7.30

---
+++ test_7_29/test_7_29/test.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1:
+			printf("星期一\n");
+			break;
+		case 2:
+           printf("星期二\n");
+	}
+	return 0;
+}
+
+//	int i = 1;
+//	while(i<101)
+//	{
+//		int a =i%2;
+//	if(a != 0)
+//		printf("%d ",i);
+//	i++;
+//	}
+//	return 0;
+//}
+//	int a = 1;
+//	int b = 2;
+//	if(a==0)
+//		if(b==2)
+//		printf("haha\n");
+//        else
+//	printf("hehe\n");
+//	return 0;
+//}
+//	if(a==1)
+//		if(b==1)
+//			printf("haha\n");
+//		else
+//		printf("hehe\n");
+//	return 0;//hehe
+//}
+	/*int age = 150;\
+		if(age<=18)
+			printf("未成年\n");
+		else if (age>=18 && age<29)
+      printf("青年\n");
+		else if(age>=29 && age<55)
+			printf("壮年\n");
+		else
+			printf("老年\n");*/
+//  return 0;
+//}
