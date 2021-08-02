21cc36a0dae3910a7471040a79beefd704d3a560
Guoliu <820267815@qq.com>
2021-08-02 17:59:50 +0800

    2021.8.2

---
+++ test_8_1/test_8_1/test.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for(i=100;i<=200;i++)
+
+//	int i = 1;
+//	int a = 0;
+//	scanf("%d",&a);
+//	for(i=2;i<=a-1;i++)
+//	{
+//      if(a%i == 0)
+//	  {
+//		  break;
+//	  }
+//	}
+//	  if(i<a-1)
+//	  {
+//		  printf("%d不是素数\n",a);
+//	  }
+//	  else
+//	  {
+//	    printf("%d是素数\n",a);
+//	  }
+//	return 0;
+//}
+
+
+
+//	int year = 0;
+//	int count = 0;
+//	for(year=1000;year<=2000;year++)
+//	{
+//		if((year%4 == 0 && year%100 !=0) || year%400 == 0)
+//			printf("%d ",year);
+//		count++;
+//	}
+//	printf("\n");
+//	printf("%d\n",count);
+//	return 0;
+//}
+
+
+
+
+
+
+//	int a = 34;
+//	int b = 7;
+//	int c = 0;
+//	while(a%b)
+//	{
+//		c=a%b;
+//		a = b;
+//		b = c;
+//	}
+//	printf("最大公约数为：%d\n",b);
+//	return 0;
+//}
+		/*int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d%d",&a,&b,&c);
+
+	printf("%d %d %d\n",a,b,c);*/
+//	return 0;
+//}
