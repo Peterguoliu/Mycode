f7f156dda84f029f201522bfc71993c020027814
Guoliu <820267815@qq.com>
2021-08-03 17:13:48 +0800

    2021.8.3

---
+++ test_8_2/test_8_2/test.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+//	int i = 0;
+//	int j = 0;
+//	for(i=1;i<=9;i++)
+//	{
+//		for(j=1;j<=i;j++)
+//		{
+//          printf("%d*%d=%-2d ",i,j,i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//	int i = 0;
+//	int arr[] ={0,1,3,76,43,87,18,56,78,98,43,90,67,89,98,56};
+//	int max = arr[0];
+//	int sz = sizeof(arr)/sizeof(arr[0]);
+//	for(i=1;i<sz;i++)
+//	{
+//		if(arr[i]>max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n",max);
+//	return 0;
+//}
+//	int i = 0;
+//    double sum =0;
+//	int flag = 1;
+//	double a = 0;
+//	for(i=1;i<=100;i++)
+//	{
+//		a = flag*1.0/i;
+//		sum += a;
+//		flag = -flag;
+//	}
+//	printf("%lf\n",sum);
+//	return 0;
+//}
+//	int i = 0;
+//	int j = 0;
+//	int count = 0;
+//	for(i=101;i<=200;i += 2)
+//	{
+//	    for(j=2;j<=sqrt(i);j++)
+//	   {
+//		   if(i%j == 0)
+//		   {
+//			   break;
+//		   }
+//	   }  
+//		   if(j>sqrt(i))
+//		   {
+//			printf("%d ",i);
+//			count++;
+//		   }
+//	}
+//	printf("\ncount= %d\n",count);
+//	return 0;
+//}
